0193721f1c86c0a2412be568a8b8a058b151d164
alexi-o <ohearnalexi@gmail.com>
2024-01-30 20:36:01 -0700

    fixes issues affecting image from displaying in template after upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 import tensorflow as tf
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decode_predictions
@@ -6,14 +6,13 @@ from PIL import Image
 import os
 
 app = Flask(__name__)
-app.config['UPLOADS_DEFAULT_DEST'] = 'uploads'  # Folder to store uploaded images
+app.config['UPLOADS_DEFAULT_DEST'] = 'uploads'
+app.config['UPLOADED_PHOTOS_DEST'] = 'uploads'
 photos = UploadSet('photos', IMAGES)
 configure_uploads(app, photos)
 
-# Load a pre-trained MobileNetV2 model
 model = MobileNetV2(weights='imagenet')
 
-# Function to process uploaded image and generate metadata
 def process_image(file):
     img = Image.open(file)
     img = img.convert('RGB')
@@ -30,12 +29,18 @@ def index():
     if request.method == 'POST' and 'photo' in request.files:
         photo = request.files['photo']
         if photo:
-            filename = photos.save(photo, folder='photos')  # Save to 'photos' subfolder
-            metadata = process_image(os.path.join(app.config['UPLOADS_DEFAULT_DEST'], 'photos', filename))
+            filename = photos.save(photo, folder=None)
+            metadata = process_image(os.path.join(app.config['UPLOADS_DEFAULT_DEST'], filename))
             flash('Metadata generated successfully!')
-            return render_template('index.html', metadata=metadata, image_url=url_for('static', filename=f'uploads/photos/{filename}'))
+
+            image_url = url_for('uploaded_file', filename=filename)
+            return render_template('index.html', metadata=metadata, image_url=image_url)
     
     return render_template('index.html', metadata=None, image_url=None)
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOADS_DEFAULT_DEST'], filename)
 
 if __name__ == '__main__':
     app.secret_key = 'your_secret_key_here'
